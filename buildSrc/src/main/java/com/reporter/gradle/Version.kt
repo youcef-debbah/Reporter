2dbebfb685449673ca5cfaf4dbb2d0797bfa03cc
jprogramer <joseph.debbah.local@gmail.com>
2023-03-29 13:28:54 +0100

    update and organise lib versions

--- buildSrc/src/main/java/com/reporter/gradle/Version.kt
+++ buildSrc/src/main/java/com/reporter/gradle/Version.kt
@@ -7,14 +7,8 @@ object Version {
     // https://truth.dev/#gradle
     const val TRUTH = "1.1.3"
 
-    // https://firebase.google.com/support/releases
-    const val FIREBASE_BOM = "31.2.3"
-
     // https://developer.android.com/jetpack/androidx/releases/core
     const val ANDROIDX_CORE = "1.9.0"
-
-    // https://github.com/material-components/material-components-android/releases
-    const val GOOGLE_MATERIAL = "1.9.0-alpha02"
 
     // https://developer.android.com/jetpack/androidx/releases/preference
     const val PREFERENCE = "1.2.0"
@@ -26,13 +20,10 @@ object Version {
     const val HILT = "2.45"
 
     // https://developer.android.com/jetpack/androidx/releases/room
-    const val ROOM = "2.5.0"
+    const val ROOM = "2.5.1"
 
     // https://developer.android.com/jetpack/androidx/releases/arch-core
     const val CORE_ARCH = "2.2.0"
-
-    // https://developer.android.com/jetpack/androidx/releases/appcompat
-    const val APPCOMPAT = "1.6.1"
 
     // https://developer.android.com/jetpack/androidx/releases/activity
     const val ACTIVITY = "1.6.1"
@@ -41,34 +32,12 @@ object Version {
     const val CUSTOMVIEW = "1.1.0"
     const val CUSTOMVIEW_POOLING = "1.0.0"
 
-    // https://developer.android.com/jetpack/androidx/releases/lifecycle
-    const val LIFECYCLE = "2.6.0-rc01"
-    const val LIFECYCLE_COMPOSE = "2.6.0-rc01"
-
     // https://developer.android.com/jetpack/compose/navigation
     const val NAVIGATION_COMPOSE = "2.5.3"
-
-    // https://developer.android.com/jetpack/androidx/releases/paging
-    const val PAGING = "3.2.0-alpha04"
-    const val PAGING_COMPOSE = "1.0.0-alpha18"
 
     // https://developer.android.com/jetpack/androidx/releases/constraintlayout
     const val CONSTRAINT_LAYOUT_COMPOSE = "1.0.1"
     const val CONSTRAINT_LAYOUT = "2.1.4"
-
-    // https://github.com/google/accompanist
-    const val COMPOSE_ACCOMPANIST = "0.28.0"
-
-    // https://developer.android.com/jetpack/androidx/releases/compose
-    object Compose {
-        const val ANIMATION = "1.3.3"
-        const val COMPILER = "1.4.3"
-        const val FOUNDATION = "1.3.1"
-        const val MATERIAL = "1.3.1"
-        const val MATERIAL3 = "1.0.1"
-        const val RUNTIME = "1.3.3"
-        const val UI = "1.3.3"
-    }
 
     // https://developer.android.com/jetpack/androidx/releases/test
     object Test {
@@ -80,6 +49,41 @@ object Version {
     // https://github.com/Tencent/MMKV
     const val MMKV = "1.2.15"
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    /* the versions below should get regulare updates becuase they are still under heavy development */
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // https://firebase.google.com/support/release-notes/android#latest_sdk_versions
+    const val FIREBASE_BOM = "31.4.0"
+
+    // https://github.com/material-components/material-components-android/releases
+    const val GOOGLE_MATERIAL = "1.9.0-beta01"
+
+    // https://developer.android.com/jetpack/androidx/releases/paging
+    const val PAGING = "3.2.0-alpha04"
+    const val PAGING_COMPOSE = "1.0.0-alpha18"
+
+    // https://developer.android.com/jetpack/androidx/releases/appcompat
+    const val APPCOMPAT = "1.7.0"
+
+    // https://developer.android.com/jetpack/androidx/releases/lifecycle
+    const val LIFECYCLE = "2.6.1"
+    const val LIFECYCLE_COMPOSE = "2.6.1"
+
     // https://github.com/alorma/Compose-Settings
-    const val COMPOSE_SETTINGS = "0.22.0"
+    const val COMPOSE_SETTINGS = "0.25.0"
+
+    // https://github.com/google/accompanist
+    const val COMPOSE_ACCOMPANIST = "0.30.0"
+
+    // https://developer.android.com/jetpack/androidx/releases/compose
+    object Compose {
+        const val ANIMATION = "1.4.0"
+        const val COMPILER = "1.4.4"
+        const val FOUNDATION = "1.4.0"
+        const val MATERIAL = "1.4.0"
+        const val MATERIAL3 = "1.0.1"
+        const val RUNTIME = "1.4.0"
+        const val UI = "1.4.0"
+    }
 }
