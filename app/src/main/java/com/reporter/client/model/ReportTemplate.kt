5280ebf438ea77f88c99d9f67b1899c33af81843
jprogramer <joseph.debbah.local@gmail.com>
2023-03-31 23:13:04 +0100

    ignore calculable field and make name index unique

--- app/src/main/java/com/reporter/client/model/ReportTemplate.kt
+++ app/src/main/java/com/reporter/client/model/ReportTemplate.kt
@@ -2,18 +2,24 @@ package com.reporter.client.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.reporter.util.model.Localizer
 
 const val REPORT_TEMPLATE_TABLE = "report_template"
 const val REPORT_TEMPLATE_ID = "report_template_id"
+const val REPORT_TEMPLATE_NAME = "name"
 
-@Entity(tableName = REPORT_TEMPLATE_TABLE)
+@Entity(
+    tableName = REPORT_TEMPLATE_TABLE,
+    indices = [Index(value = [REPORT_TEMPLATE_NAME], unique = true)]
+)
 data class ReportTemplate(
     @PrimaryKey
     @ColumnInfo(name = REPORT_TEMPLATE_ID)
     val id: Long,
-    @ColumnInfo(index = true)
+    @ColumnInfo(name = REPORT_TEMPLATE_NAME)
     val name: String,
     val content: String,
     val label_en: String?,
@@ -23,10 +29,12 @@ data class ReportTemplate(
     val desc_ar: String?,
     val desc_fr: String?,
 ) {
+    @delegate:Ignore
     val label by lazy {
         Localizer.inPrimaryLang(label_en, label_ar, label_fr)
     }
 
+    @delegate:Ignore
     val desc by lazy {
         Localizer.inPrimaryLang(desc_en, desc_ar, desc_fr)
     }
