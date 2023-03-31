8f0d4f88c643aab7cbddce7a647eae949efd4f46
jprogramer <joseph.debbah.local@gmail.com>
2023-03-31 19:53:41 +0100

    make abstract destination actually abstract and add a basic concrete implementation to increase design flexibility

--- app/src/main/java/com/reporter/util/ui/AbstractDestination.kt
+++ app/src/main/java/com/reporter/util/ui/AbstractDestination.kt
@@ -2,36 +2,53 @@ package com.reporter.util.ui
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * a destination can be a screen or a graph of screens
  */
 @Immutable
-open class AbstractDestination(
+abstract class AbstractDestination(
     /**
      * the destination's unique route
      */
     val route: String,
     /**
      * used as the icon for any button that opens this destination
-     * also -it this destination is a screen- it may be shown at the top right next to the title
+     * also -if this destination is a screen- it may be shown at the top right next to the title
      */
-    @DrawableRes val icon: Int,
+    @DrawableRes val icon: Int?,
+) {
+
     /**
      * used as an icon description for an icon button that opens this destination
      * also -it this destination is a screen- it may be shown at the top (for example inside an app bar)
      */
-    @StringRes val title: Int,
+    @Composable
+    @ReadOnlyComposable
+    abstract fun title(): String
+
     /**
      * used as the text for any button that opens this destination
      */
-    @StringRes val label: Int? = null,
-) {
+    @Composable
+    @ReadOnlyComposable
+    abstract fun label(): String?
+
+    /**
+     * used to construct the navigation bar UI for this screen, if this screen is included it will
+     * be shown as "selected" in the UI, an empty array will cause the navigation bar to be hidden
+     */
+    abstract val destinations: List<AbstractDestination>
 
     override fun toString(): String = route
 
-    override fun equals(other: Any?): Boolean {
+    final override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is AbstractDestination) return false
 
@@ -40,7 +57,37 @@ open class AbstractDestination(
         return true
     }
 
-    override fun hashCode(): Int {
+    final override fun hashCode(): Int {
         return route.hashCode()
     }
 }
+
+@Immutable
+open class StaticScreenDestination(
+    route: String,
+    @DrawableRes icon: Int?,
+    @StringRes val titleRes: Int,
+    @StringRes val labelRes: Int? = null,
+) : AbstractDestination(route, icon) {
+
+    @Composable
+    @ReadOnlyComposable
+    override fun title(): String = LocalContext.current.getString(titleRes)
+
+    @Composable
+    @ReadOnlyComposable
+    override fun label(): String? = labelRes?.let { LocalContext.current.getString(labelRes) }
+
+    override val destinations: List<AbstractDestination>
+        get() = emptyList()
+}
+
+val activeScreens = ConcurrentHashMap<String, AbstractDestination>(8)
+
+fun NavHostController.currentScreen(): AbstractDestination? {
+    val currentRoute = currentDestination?.route
+    return if (currentRoute == null) null else activeScreens[currentRoute]
+}
+
+fun AbstractDestination?.destinationsOrEmpty(): List<AbstractDestination> =
+    this?.destinations ?: emptyList()
