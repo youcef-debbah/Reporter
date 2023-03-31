cdc6aacb4e389188bbb492d359ecf207200383ce
jprogramer <joseph.debbah.local@gmail.com>
2023-03-31 20:48:18 +0100

    add a navigation scaffold with a WebView around TemplatesListScreen

--- app/src/main/java/com/reporter/client/ui/MainActivity.kt
+++ app/src/main/java/com/reporter/client/ui/MainActivity.kt
@@ -1,24 +1,40 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalMaterialNavigationApi::class,
+)
 
 package com.reporter.client.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import com.reporter.util.model.Teller
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.reporter.client.ui.TemplatesListScreen.addTemplatesListScreen
 import com.reporter.util.ui.AbstractActivity
-import com.reporter.util.ui.SimpleScaffold
+import com.reporter.util.ui.NavigationBarScaffold
+import com.reporter.util.ui.addStandardAppBarScreens
 
 class MainActivity : AbstractActivity() {
 
     override fun onCreate(state: Bundle?) {
         super.onCreate(state)
+
+        val webView = createWebView()
+
         setContent {
-            SimpleScaffold {
-                Teller.info("teller in action!")
-                Text("reporter in action!")
+            NavigationBarScaffold(TemplatesListScreen) { navController ->
+                addStandardAppBarScreens(navController)
+                addTemplatesListScreen(navController, webView)
             }
         }
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun createWebView(): WebView =
+        WebView(this).also {
+            it.settings.apply {
+                javaScriptEnabled = true
+            }
+        }
 }
