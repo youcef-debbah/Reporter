73307cfe664b82058c60d741abdc26af81fe6df8
jprogramer <joseph.debbah.local@gmail.com>
2023-04-10 20:42:36 +0100

    refactor toasts utils for better fun namespacing

--- app/src/main/java/com/reporter/util/ui/Toasts.kt
+++ app/src/main/java/com/reporter/util/ui/Toasts.kt
@@ -6,27 +6,29 @@ import androidx.annotation.StringRes
 import com.reporter.common.PublicAPI
 import com.reporter.client.R
 
-@PublicAPI
-fun shortToast(text: String, context: Context = AbstractApplication.INSTANCE) {
-    Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
-}
+object Toasts {
+    @PublicAPI
+    fun short(text: String, context: Context = AbstractApplication.INSTANCE) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
 
-@PublicAPI
-fun shortToast(@StringRes text: Int, context: Context = AbstractApplication.INSTANCE) {
-    Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
-}
+    @PublicAPI
+    fun short(@StringRes text: Int, context: Context = AbstractApplication.INSTANCE) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
 
-@PublicAPI
-fun longToast(text: String, context: Context = AbstractApplication.INSTANCE) {
-    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
-}
+    @PublicAPI
+    fun long(text: String, context: Context = AbstractApplication.INSTANCE) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+    }
 
-@PublicAPI
-fun longToast(@StringRes text: Int, context: Context = AbstractApplication.INSTANCE) {
-    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
-}
+    @PublicAPI
+    fun long(@StringRes text: Int, context: Context = AbstractApplication.INSTANCE) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+    }
 
-@PublicAPI
-fun toastNotImplemented() {
-    shortToast(R.string.not_implemented_yet)
+    @PublicAPI
+    fun notImplemented() {
+        short(R.string.not_implemented_yet)
+    }
 }
