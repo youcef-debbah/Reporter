b37d7c6b29d11b25b4fed24deb65e53749f80b1e
jprogramer <joseph.debbah.local@gmail.com>
2023-04-10 20:51:57 +0100

    inject the view model manual on activity start to trigger repo initializing earlier

--- app/src/main/java/com/reporter/client/ui/MainActivity.kt
+++ app/src/main/java/com/reporter/client/ui/MainActivity.kt
@@ -4,12 +4,10 @@
 
 package com.reporter.client.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebView
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.lifecycle.ViewModelProvider
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.reporter.client.model.MainViewModel
 import com.reporter.client.ui.TemplatesListScreen.addTemplatesListScreen
@@ -21,26 +19,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AbstractActivity() {
 
-    val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(state: Bundle?) {
         super.onCreate(state)
-
-        val webView = createWebView()
-
         setContent {
+            val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
             NavigationBarScaffold(TemplatesListScreen) { navController ->
                 addStandardAppBarScreens(navController)
-                addTemplatesListScreen(navController, webView, viewModel)
+                addTemplatesListScreen(navController, viewModel)
             }
         }
     }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun createWebView(): WebView =
-        WebView(this).also {
-            it.settings.apply {
-                javaScriptEnabled = true
-            }
-        }
 }
