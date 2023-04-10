a245845bd2101909db4c55f0fbfeb8791f65ec84
jprogramer <joseph.debbah.local@gmail.com>
2023-04-10 20:56:12 +0100

    add a remove config to set the debounce delay of template previews

--- app/src/main/java/com/reporter/client/model/ReporterConfig.kt
+++ app/src/main/java/com/reporter/client/model/ReporterConfig.kt
@@ -1,13 +1,15 @@
 package com.reporter.client.model
 
 import com.google.common.collect.ImmutableMap
+import com.reporter.util.model.RemoteConfig
+import com.reporter.util.model.putConfig
+
+val CONFIG_TEMPLATE_PREVIEW_DEBOUNCE = RemoteConfig<Long>("TEMPLATE_PREVIEW_DEBOUNCE", 500)
 
 val ALL_REMOTE_REPORTER_CONFIGS: ImmutableMap<String, Any> = ImmutableMap.builder<String, Any>()
+    .putConfig(CONFIG_TEMPLATE_PREVIEW_DEBOUNCE)
     .build()
-
-//val CONFIG_NAME = LocalConfig("CONFIG_FULL_NAME", "default")
 
 @Suppress("unused")
 val ALL_LOCAL_REPORTER_CONFIGS: ImmutableMap<String, Any> = ImmutableMap.builder<String, Any>()
-//    .putConfig(CONFIG_NAME)
     .build()
