4ab5d763df9d26b9c81ccb8fc4940949a22bde6d
jprogramer <joseph.debbah.local@gmail.com>
2023-05-12 12:42:12 +0100

    add a simple api to check for pre conditions

---
+++ core/src/main/java/dz/nexatech/reporter/client/common/Preconditions.kt
@@ -0,0 +1,115 @@
+@file:JvmMultifileClass
+@file:JvmName("PreconditionsKt")
+@file:OptIn(ExperimentalContracts::class)
+
+package dz.nexatech.reporter.client.common
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+/**
+ * Throws an [IllegalArgumentException] if the [value] is false.
+ */
+fun require(value: Boolean) {
+    contract {
+        returns() implies value
+    }
+    require(value) { "Failed requirement." }
+}
+
+/**
+ * Throws an [IllegalArgumentException] with the result of calling [lazyMessage] if the [value] is false.
+ */
+inline fun require(value: Boolean, lazyMessage: () -> Any) {
+    contract {
+        returns() implies value
+    }
+    if (!value) {
+        val message = lazyMessage()
+        throw IllegalArgumentException(message.toString())
+    }
+}
+
+/**
+ * Throws an [IllegalArgumentException] if the [value] is null. Otherwise returns the not null value.
+ */
+fun <T : Any> requireNotNull(value: T?): T {
+    contract {
+        returns() implies (value != null)
+    }
+    return requireNotNull(value) { "Required value was null." }
+}
+
+/**
+ * Throws an [IllegalArgumentException] with the result of calling [lazyMessage] if the [value] is null. Otherwise
+ * returns the not null value.
+ */
+inline fun <T : Any> requireNotNull(value: T?, lazyMessage: () -> Any): T {
+    contract {
+        returns() implies (value != null)
+    }
+
+    if (value == null) {
+        val message = lazyMessage()
+        throw IllegalArgumentException(message.toString())
+    } else {
+        return value
+    }
+}
+
+/**
+ * Throws an [IllegalStateException] if the [value] is false.
+ */
+fun check(value: Boolean): Unit {
+    contract {
+        returns() implies value
+    }
+    check(value) { "Check failed." }
+}
+
+/**
+ * Throws an [IllegalStateException] with the result of calling [lazyMessage] if the [value] is false.
+ */
+inline fun check(value: Boolean, lazyMessage: () -> Any): Unit {
+    contract {
+        returns() implies value
+    }
+    if (!value) {
+        val message = lazyMessage()
+        throw IllegalStateException(message.toString())
+    }
+}
+
+/**
+ * Throws an [IllegalStateException] if the [value] is null. Otherwise
+ * returns the not null value.
+ */
+fun <T : Any> checkNotNull(value: T?): T {
+    contract {
+        returns() implies (value != null)
+    }
+    return checkNotNull(value) { "Required value was null." }
+}
+
+/**
+ * Throws an [IllegalStateException] with the result of calling [lazyMessage]  if the [value] is null. Otherwise
+ * returns the not null value.
+ */
+inline fun <T : Any> checkNotNull(value: T?, lazyMessage: () -> Any): T {
+    contract {
+        returns() implies (value != null)
+    }
+
+    if (value == null) {
+        val message = lazyMessage()
+        throw IllegalStateException(message.toString())
+    } else {
+        return value
+    }
+}
+
+
+/**
+ * Throws an [IllegalStateException] with the given [message].
+ */
+fun error(message: Any): Nothing = throw IllegalStateException(message.toString())
