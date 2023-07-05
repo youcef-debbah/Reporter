ebb2ce90d3a7444545074a0e364de86e58a6077a
jprogramer <joseph.debbah.local@gmail.com>
2023-07-05 19:22:19 +0100

    refactor common code from Localizer to AbstractLocalizer

--- app/src/main/java/dz/nexatech/reporter/util/model/Localizer.kt
+++ app/src/main/java/dz/nexatech/reporter/util/model/Localizer.kt
@@ -2,7 +2,6 @@ package dz.nexatech.reporter.util.model
 
 import android.os.Build
 import com.google.common.collect.ImmutableList
-import com.google.common.collect.ImmutableMap
 import dz.nexatech.reporter.client.common.AbstractLocalizer
 import dz.nexatech.reporter.client.common.Texts
 import dz.nexatech.reporter.client.common.atomicLazy
@@ -133,102 +132,7 @@ class Localizer private constructor(formattingLang: String?) : AbstractLocalizer
 
     companion object {
 
-        val ARABIC_LOCALE: Locale = Locale("ar", "DZ")
-        val FRENCH_LOCALE: Locale = Locale.FRANCE
-        val ENGLISH_LOCALE: Locale = Locale.UK
-
-        val utcTimeZone: TimeZone = TimeZone.getTimeZone("UTC")
-
-        fun newCalendar(): Calendar = Calendar.getInstance(utcTimeZone)
-
         fun from(lang: String): Localizer = Localizer(lang)
-
-        private val monthsIndexes = ImmutableMap.builder<String, Int>()
-            .put("January", 0)
-            .put("February", 1)
-            .put("March", 2)
-            .put("April", 3)
-            .put("May", 4)
-            .put("June", 5)
-            .put("July", 6)
-            .put("August", 7)
-            .put("September", 8)
-            .put("October", 9)
-            .put("November", 10)
-            .put("December", 11)
-
-            .put("Janvier", 0)
-            .put("Février", 1)
-            .put("Mars", 2)
-            .put("Avril", 3)
-            .put("Mai", 4)
-            .put("Juin", 5)
-            .put("Juillet", 6)
-            .put("Août", 7)
-            .put("Septembre", 8)
-            .put("Octobre", 9)
-            .put("Novembre", 10)
-            .put("Décembre", 11)
-
-            .put("\u062C\u0627\u0646\u0641\u064A", 0)
-            .put("\u0641\u064A\u0641\u0631\u064A", 1)
-            .put("\u0645\u0627\u0631\u0633", 2)
-            .put("\u0623\u0641\u0631\u064A\u0644", 3)
-            .put("\u0645\u0627\u064A", 4)
-            .put("\u062C\u0648\u0627\u0646", 5)
-            .put("\u062C\u0648\u064A\u0644\u064A\u0629", 6)
-            .put("\u0623\u0648\u062A", 7)
-            .put("\u0633\u0628\u062A\u0645\u0628\u0631", 8)
-            .put("\u0623\u0643\u062A\u0648\u0628\u0631", 9)
-            .put("\u0646\u0648\u0641\u0645\u0628\u0631", 10)
-            .put("\u062F\u064A\u0633\u0645\u0628\u0631", 11)
-
-            .build()
-
-        private val englishMonths = arrayOf(
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        )
-
-        private val frenchMonths = arrayOf(
-            "Janvier",
-            "Février",
-            "Mars",
-            "Avril",
-            "Mai",
-            "Juin",
-            "Juillet",
-            "Août",
-            "Septembre",
-            "Octobre",
-            "Novembre",
-            "Décembre",
-        )
-
-        private val arabicMonths = arrayOf(
-            "\u062C\u0627\u0646\u0641\u064A",
-            "\u0641\u064A\u0641\u0631\u064A",
-            "\u0645\u0627\u0631\u0633",
-            "\u0623\u0641\u0631\u064A\u0644",
-            "\u0645\u0627\u064A",
-            "\u062C\u0648\u0627\u0646",
-            "\u062C\u0648\u064A\u0644\u064A\u0629",
-            "\u0623\u0648\u062A",
-            "\u0633\u0628\u062A\u0645\u0628\u0631",
-            "\u0623\u0643\u062A\u0648\u0628\u0631",
-            "\u0646\u0648\u0641\u0645\u0628\u0631",
-            "\u062F\u064A\u0633\u0645\u0628\u0631",
-        )
 
         private val preferredLanguages: ImmutableList<String> by atomicLazy {
             val result = LinkedList<String>()
@@ -284,6 +188,5 @@ class Localizer private constructor(formattingLang: String?) : AbstractLocalizer
             frenchText: String?,
             arabicText: String?,
         ): String = Texts.inSecondaryLang(englishText, frenchText, arabicText, preferredLanguages)
-
     }
 }
--- core/src/main/java/dz/nexatech/reporter/client/common/AbstractLocalizer.kt
+++ core/src/main/java/dz/nexatech/reporter/client/common/AbstractLocalizer.kt
@@ -1,6 +1,9 @@
 package dz.nexatech.reporter.client.common
 
+import com.google.common.collect.ImmutableMap
+import java.util.Calendar
 import java.util.Locale
+import java.util.TimeZone
 
 abstract class AbstractLocalizer {
 
@@ -19,7 +22,7 @@ abstract class AbstractLocalizer {
     abstract fun inPrimaryLang(
         englishText: String?,
         frenchText: String?,
-        arabicText: String?
+        arabicText: String?,
     ): String
 
     abstract fun inSecondaryLang(latinText: String?, arabicText: String?): String
@@ -27,6 +30,116 @@ abstract class AbstractLocalizer {
     abstract fun inSecondaryLang(
         englishText: String?,
         frenchText: String?,
-        arabicText: String?
+        arabicText: String?,
     ): String
+
+    companion object {
+
+        val ARABIC_LOCALE: Locale = Locale("ar", "DZ")
+        val FRENCH_LOCALE: Locale = Locale.FRANCE
+        val ENGLISH_LOCALE: Locale = Locale.UK
+
+        val utcTimeZone: TimeZone = TimeZone.getTimeZone("UTC")
+
+        fun newCalendar(): Calendar = Calendar.getInstance(utcTimeZone)
+
+        fun newCalendar(year: Int, month: Int, day: Int): Calendar =
+            newCalendar().apply {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, month)
+                set(Calendar.DAY_OF_MONTH, day)
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+                add(Calendar.DAY_OF_MONTH, 1)
+            }
+
+        val monthsIndexes = ImmutableMap.builder<String, Int>()
+            .put("January", 0)
+            .put("February", 1)
+            .put("March", 2)
+            .put("April", 3)
+            .put("May", 4)
+            .put("June", 5)
+            .put("July", 6)
+            .put("August", 7)
+            .put("September", 8)
+            .put("October", 9)
+            .put("November", 10)
+            .put("December", 11)
+
+            .put("Janvier", 0)
+            .put("Février", 1)
+            .put("Mars", 2)
+            .put("Avril", 3)
+            .put("Mai", 4)
+            .put("Juin", 5)
+            .put("Juillet", 6)
+            .put("Août", 7)
+            .put("Septembre", 8)
+            .put("Octobre", 9)
+            .put("Novembre", 10)
+            .put("Décembre", 11)
+
+            .put("\u062C\u0627\u0646\u0641\u064A", 0)
+            .put("\u0641\u064A\u0641\u0631\u064A", 1)
+            .put("\u0645\u0627\u0631\u0633", 2)
+            .put("\u0623\u0641\u0631\u064A\u0644", 3)
+            .put("\u0645\u0627\u064A", 4)
+            .put("\u062C\u0648\u0627\u0646", 5)
+            .put("\u062C\u0648\u064A\u0644\u064A\u0629", 6)
+            .put("\u0623\u0648\u062A", 7)
+            .put("\u0633\u0628\u062A\u0645\u0628\u0631", 8)
+            .put("\u0623\u0643\u062A\u0648\u0628\u0631", 9)
+            .put("\u0646\u0648\u0641\u0645\u0628\u0631", 10)
+            .put("\u062F\u064A\u0633\u0645\u0628\u0631", 11)
+
+            .build()
+
+        val englishMonths = arrayOf(
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        )
+
+        val frenchMonths = arrayOf(
+            "Janvier",
+            "Février",
+            "Mars",
+            "Avril",
+            "Mai",
+            "Juin",
+            "Juillet",
+            "Août",
+            "Septembre",
+            "Octobre",
+            "Novembre",
+            "Décembre",
+        )
+
+        val arabicMonths = arrayOf(
+            "\u062C\u0627\u0646\u0641\u064A",
+            "\u0641\u064A\u0641\u0631\u064A",
+            "\u0645\u0627\u0631\u0633",
+            "\u0623\u0641\u0631\u064A\u0644",
+            "\u0645\u0627\u064A",
+            "\u062C\u0648\u0627\u0646",
+            "\u062C\u0648\u064A\u0644\u064A\u0629",
+            "\u0623\u0648\u062A",
+            "\u0633\u0628\u062A\u0645\u0628\u0631",
+            "\u0623\u0643\u062A\u0648\u0628\u0631",
+            "\u0646\u0648\u0641\u0645\u0628\u0631",
+            "\u062F\u064A\u0633\u0645\u0628\u0631",
+        )
+    }
 }
