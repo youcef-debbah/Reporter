39e7625d39473ef4d1544f45b66a27d5f7a49938
jprogramer <joseph.debbah.local@gmail.com>
2023-07-05 20:25:26 +0100

    fix a bug when creating a new calendar instance

--- core/src/main/java/dz/nexatech/reporter/client/common/AbstractLocalizer.kt
+++ core/src/main/java/dz/nexatech/reporter/client/common/AbstractLocalizer.kt
@@ -52,7 +52,6 @@ abstract class AbstractLocalizer {
                 set(Calendar.MINUTE, 0)
                 set(Calendar.SECOND, 0)
                 set(Calendar.MILLISECOND, 0)
-                add(Calendar.DAY_OF_MONTH, 1)
             }
 
         val monthsIndexes = ImmutableMap.builder<String, Int>()
