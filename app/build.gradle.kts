44f929afa539ef5e158ff58537ac01bd4d0f8da8
jprogramer <joseph.debbah.local@gmail.com>
2023-08-03 19:48:52 +0100

    bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 
 logger.info("app build config...")
 
-standardAndroidApp("dz.nexatech.reporter.client", 18, "proguard-rules.pro")
+standardAndroidApp("dz.nexatech.reporter.client", 19, "proguard-rules.pro")
 
 dependencies {
     addCommonTestDependencies()
